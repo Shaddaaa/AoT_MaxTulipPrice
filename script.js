965c2a229f9e9464618504a5a4dad51b3f954da4
Shaddaaa <florian@kaufmann.cloud>
2020-08-09 13:55:15 +0200

    added a button to max the tulip price, instead of relying on refreshing the page

--- script.js
+++ script.js
@@ -8,7 +8,22 @@ ShaddasMaxTulipPrice.setMaxPrice = function() {
 	priceInput.value = maxPrice;
 }
 
+ShaddasMaxTulipPrice.setupUI = function() {
+    let parent = document.getElementById("main-container").children[2].children[0].children[1].children[0].children[0].children[0].children[1].children[1].children[0];
+
+    let button = document.createElement("button");
+    button.onclick = () => {
+        ShaddasMaxTulipPrice.setMaxPrice();
+    }
+
+    button.innerHTML = "Max Tulip price"
+    button.classList.add("btn");
+    button.classList.add("btn-sm");
+    button.classList.add("btn-primary");
+    parent.insertBefore(button, parent.children[2]);
+}
+
 //only call setMaxPrice if you are one the correct page AND if you are the owner of the tulip
 if (window.location.href == "https://ageoftrades.com/game" && document.getElementById("main-container").children[2].children[0].children[1].children[0].children[0].children[0].children[1].children[0].innerHTML == "You are the current owner of the tulip. You need to set a price.") {
-	ShaddasMaxTulipPrice.setMaxPrice();
+	ShaddasMaxTulipPrice.setupUI();
 }
